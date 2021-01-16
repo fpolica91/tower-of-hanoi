1f41d7d185e2cd2c9f0d518e8c436e0840216725
Fabricio <fabriciopolicarpo0@gmail.com>
2021-01-16 02:08:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+void TOH(int n, int A, int B, int C){
+  if(n > 0){
+    TOH(n-1, A, C, B);
+    printf("from %d to %d ", A, C);
+    TOH(n-1, B, A, C);
+  }
+}
+
+int main() {
+  TOH(3, 1, 2, 3);
+  std::cout << "Hello World!\n";
+}
